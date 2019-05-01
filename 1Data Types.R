1a13bbbd903018130e1cc8cf7ddfc90629cf493b
SurenderSingh04 <surender120992@gmail.com>
2019-05-01 19:05:27 +0530

    first file

---
+++ 1Data Types.R
@@ -0,0 +1,39 @@
+#Data Type
+
+a=10
+class(a)
+
+#Type Casting
+
+a=as.numeric(a)
+class(a)
+a=as.character(a)
+class(a)
+a=as.integer(a)
+class(a)
+
+a="10.5"
+class(a)  #character
+a=as.numeric(a)
+class(a)  # Numeric
+
+# if character values contain alphabets then we can't type cast them to integer/numeric
+
+a='Apple'
+a=as.numeric(a)  #It will not be able to convert a character value with alphabets into numeric. But it will convert the data type of a to numeric.
+class(a)  # type is numeric
+
+
+#verifying a data type
+
+is.numeric(a)  #datatype of a is numeric. So it returns TRUE
+is.character(a)  # FALSE
+
+#Boolean data Types
+a=TRUE
+b=T
+k=F
+
+class(a)
+class(b)
+class(k)
